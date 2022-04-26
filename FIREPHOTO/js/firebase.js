235acc5f4dd466047215c8b9f69973f51776e57d
Katalin Varga <79935601+KatalinVarga@users.noreply.github.com>
2022-04-26 15:59:04 +0100

    Add files via upload

---
+++ FIREPHOTO/js/firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDR9PMcDpRZRtwJ4d8HGTloiNoc80m3pnU",
+    authDomain: "firephoto-b5909.firebaseapp.com",
+    projectId: "firephoto-b5909",
+    storageBucket: "firephoto-b5909.appspot.com",
+    messagingSenderId: "9194437529",
+    appId: "1:9194437529:web:674c316365eb873197b108",
+});
+
+var db = firebase.firestore();
+var storage = firebase.storage(); // these are functions and methods, loaded in 
