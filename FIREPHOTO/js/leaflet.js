54e72cddf0d809593af6905b6891282d887bb6ca
Katalin Varga <79935601+KatalinVarga@users.noreply.github.com>
2022-04-29 16:03:29 +0100

    Add files via upload

--- FIREPHOTO/js/leaflet.js
+++ FIREPHOTO/js/leaflet.js
@@ -11,4 +11,45 @@ document.addEventListener('DOMContentLoaded',()=>{
     accessToken: 'pk.eyJ1Ijoia2F0YWxpbnYiLCJhIjoiY2t6YnZxZGZjMXRzajJubnhkaHBhemlpaiJ9.vogKmo-xezTaHkwMRuLDjg'
 }).addTo(map);
 
+    db.collection('locations').get().then((querysnapshot)=>{
+        querysnapshot.forEach((doc) =>{
+            console.log(doc.data());
+            var loc = doc.data();
+            var marker = L.marker([loc.lat, loc.lng]).addTo(map);
+        })
+    })
+
+    var modalElems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(modalElems);
+
+    var addImageModal = document.querySelector('#addImageModal');
+    var instance = M.Modal.getInstance(addImageModal);
+
+    var latPlaceboholder=document.querySelector('#latPlaceboholder');
+    var lngPlaceboholder=document.querySelector('#lngPlaceboholder');
+
+map.on('click', clickedMap);
+
+function clickedMap(event){
+
+    // instance.open();
+    
+    // console.log(event);
+    
+    // console.log(event.latlng.lat, event.latlng.lng);  // this syntax is referred to as a 'period' (in the US)  -- it is called traverse an object, a dot notation (the name of the method)
+
+   latPlaceboholder.textContent = event.latlng.lat;
+   lngPlaceboholder.textContent = event.latlng.lng;
+
+   db.collection('locations').doc().set({
+
+       lat: event.latlng.lat,
+       lng:event.latlng.lng
+
+   }).then(()=>{
+       location.reload();
+   })
+    }   
+
 })
+
